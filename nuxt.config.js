26a77e55f7545c23b09b1942a1cc2baa89e95431
natlec <git@natlec.ch>
2021-05-01 17:51:17 +1000

    Add full URLs for metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,11 @@ export default {
       { hid: 'description', name: 'description', content: 'We are EWBMQ, a newly-formed chapter of EWB at Macquarie University. We aim to bring awareness and create social value through humanitarian and sustainable engineering through a variety of events, including: School Outreach Programs, Hackathons, Design Summits and much more.' },
       { hid: 'twitter:title', name: 'twitter:title', content: 'EWBMQ — Engineers Without Borders Macquarie University' },
       { hid: 'twitter:description', name: 'twitter:description', content: 'We are EWBMQ, a newly-formed chapter of EWB at Macquarie University. We aim to bring awareness and create social value through humanitarian and sustainable engineering through a variety of events, including: School Outreach Programs, Hackathons, Design Summits and much more.' },
-      { hid: 'twitter:image', name: 'twitter:image', content: '/social.png' },
+      { hid: 'twitter:image', name: 'twitter:image', content: 'https://ewbmq.org/social.png' },
       { hid: 'og:type', content: 'website' },
       { hid: 'og:title', name: 'og:title', content: 'EWBMQ — Engineers Without Borders Macquarie University' },
       { hid: 'og:description', name: 'og:description', content: 'We are EWBMQ, a newly-formed chapter of EWB at Macquarie University. We aim to bring awareness and create social value through humanitarian and sustainable engineering through a variety of events, including: School Outreach Programs, Hackathons, Design Summits and much more.' },
-      { hid: 'og:image', name: 'og:image', content: '/social.png' },
+      { hid: 'og:image', name: 'og:image', content: 'https://ewbmq.org/social.png' },
       { hid: 'og:url', name: 'og:url', content: 'https://ewbmq.org' }
     ],
     link: [
