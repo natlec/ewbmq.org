fcbe446b75c9dba66d9b0f68c812968204cc2b31
natlec <git@natlec.ch>
2021-08-19 16:58:46 +1000

    Close registrations for International Games Night event

--- pages/events/games-night.vue
+++ pages/events/games-night.vue
@@ -4,23 +4,17 @@
       <header class="c-header">
         <h1 class="c-header-title">
           <span class="c-header-subtitle">Event Alert:</span>
-          International Games Night 2021
+          International Games Night
         </h1>
       </header>
 
       <section class="c-section c-section--thinTopMargin c-section--thinBottomMargin c-section--thin">
         <p>
-          Bored during the lockdown? Come join our International Games Night on <strong>Thursday, 19th of August at 7:30pm</strong> to go head-to-head with our folks from <a href="https://instagram.com/ewbmum/" target="_blank" rel="noreferrer noopener" title="EWB Monash Malaysia on Instagram">EWB Monash Malaysia</a>! This is a great opportunity to make new connections across cultures, have fun and win awesome prizes! 🎮🤗
+          <strong>Registrations for the International Games Night on Thursday, 19th of August at 7:30pm are now closed.</strong> If you've already registered, keep an eye out for the invite link that'll be sent to your email an hour before the event.
         </p>
         <div class="c-info">
-          <strong>Note:</strong> You can either sign-up as a team with your friends or you can sign-up individually and meet new people! Need help? Feel free to ping the friendly EWBMQ team by <a href="mailto:macquarie@ewb.org.au" target="_blank" rel="noreferrer noopener" title="Compose a new email to EWBMQ in your default email application">sending us an email</a>.
+          <strong>Need help?</strong> Feel free to ping the friendly EWBMQ team by <a href="mailto:macquarie@ewb.org.au" target="_blank" rel="noreferrer noopener" title="Compose a new email to EWBMQ in your default email application">sending us an email</a>.
         </div>
-      </section>
-
-      <section class="c-section c-section--thinTopMargin c-section--thin">
-        <client-only>
-          <Form form-id="39d98305-eb4a-4b7d-9b06-1e978a198c8b" />
-        </client-only>
       </section>
 
       <section class="c-footerLink">
@@ -35,9 +29,6 @@
 
 <script>
 export default {
-  components: {
-    Form: () => process.client ? import(/* webpackPrefetch: true */ '~/lazy-components/Form.vue') : null
-  },
   head: {
     title: 'Event Alert: International Games Night 2021 — EWBMQ',
     meta: [
