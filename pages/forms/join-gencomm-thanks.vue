8365a829898bbd12a02fbe1d48c667609da926e3
natlec <git@natlec.ch>
2022-03-01 18:25:39 +1100

    Add form for GenComms applications

---
+++ pages/forms/join-gencomm-thanks.vue
@@ -0,0 +1,187 @@
+<template>
+  <main>
+    <div id="content" class="content">
+      <header class="c-header">
+        <h1 class="c-header-title">
+          <span class="c-header-subtitle">Congratulations:</span>
+          Application submitted!
+        </h1>
+      </header>
+
+      <section class="c-section c-section--center c-section--thinTopMargin c-section--thinBottomMargin c-section--thin">
+        <p>
+          <strong>Thanks for showing an interest in joining the EWBMQ general committee!</strong> You've automatically been added to the applications register, you'll be given an opportunity to chat with someone from our team in a follow-up email by scheduling a time/date. If you haven't already done so, feel free to keep in touch for other update via our other channels below:
+        </p>
+        <ul class="c-socials c-socials--center">
+          <li class="c-socials-item">
+            <a class="c-socials-item-link" href="https://www.instagram.com/ewbmacquarie" target="_blank" rel="noreferrer noopener" title="Instagram">
+              <img loading="lazy" class="c-socials-item-image" src="~/assets/icons/icon-instagram.svg" alt="Instagram" width="48">
+              <span class="c-socials-item-label">
+                Instagram
+              </span>
+            </a>
+          </li>
+          <li class="c-socials-item">
+            <a class="c-socials-item-link" href="https://www.facebook.com/ewbmq" target="_blank" rel="noreferrer noopener" title="Facebook">
+              <img loading="lazy" class="c-socials-item-image" src="~/assets/icons/icon-facebook.svg" alt="Facebook" width="48">
+              <span class="c-socials-item-label">
+                Facebook
+              </span>
+            </a>
+          </li>
+          <li class="c-socials-item">
+            <a class="c-socials-item-link" href="https://discord.gg/HGxBsCq" target="_blank" rel="noreferrer noopener" title="Discord">
+              <img loading="lazy" class="c-socials-item-image" src="~/assets/icons/icon-discord.svg" alt="Discord" width="48">
+              <span class="c-socials-item-label">
+                Discord
+              </span>
+            </a>
+          </li>
+          <li class="c-socials-item">
+            <a class="c-socials-item-link" href="https://www.twitter.com/ewbmq" target="_blank" rel="noreferrer noopener" title="Twitter">
+              <img loading="lazy" class="c-socials-item-image" src="~/assets/icons/icon-twitter.svg" alt="Twitter" width="48">
+              <span class="c-socials-item-label">
+                Twitter
+              </span>
+            </a>
+          </li>
+        </ul>
+      </section>
+
+      <section class="c-footerLink">
+        <NuxtLink to="/" class="c-footerLink-button">
+          <span class="c-footerLink-label">Go to EWBMQ homepage</span>
+        </NuxtLink>
+      </section>
+    </div>
+    <Footer />
+  </main>
+</template>
+
+<script>
+export default {
+  head: {
+    title: "Congratulations: You're nominated! — EWBMQ",
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'robots', content: 'noindex' }
+    ]
+  }
+}
+</script>
+
+<style scoped>
+.c-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  height: 175px;
+  background: var(--color-background);
+  box-shadow: 0 0 0 2px rgba(0, 0, 0, .05),
+              0 4px 32px rgba(0, 0, 0, .05);
+}
+.c-header-title {
+  font-size: 32px;
+  line-height: 48px;
+  text-align: center;
+}
+.c-header-subtitle {
+  display: block;
+  font-size: 20px;
+  line-height: 24px;
+  color: var(--color-green);
+}
+
+.c-socials {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  margin: 32px 0 0;
+  padding: 0;
+  align-items: center;
+  justify-content: center;
+}
+.c-socials-item {
+  display: block;
+  margin: 24px;
+  flex: 0 0 1;
+}
+.c-socials-item-link {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: var(--color-black);
+  text-decoration: none;
+}
+.c-socials-item-image {
+  display: block;
+  position: relative;
+  border-radius: 100%;
+  width: 48px;
+  height: 48px;
+  background: var(--color-black);
+  border: 3px solid var(--color-black);
+  outline: none;
+  transition: box-shadow .2s ease;
+}
+.c-socials-item-link:focus {
+  box-shadow: none;
+  background: var(--color-background);
+  color: var(--color-black);
+}
+.c-socials-item-link:hover .c-socials-item-image {
+  border-color: var(--color-black);
+  box-shadow: inset 0 0 0 3px var(--color-dark-gray);
+}
+.c-socials-item-link:focus .c-socials-item-image {
+  background: var(--color-green);
+  border-color: var(--color-light-gray);
+  box-shadow: 0 0 0 4px var(--color-black);
+}
+.c-socials-item-label {
+  display: block;
+  margin: 16px;
+  font-family: 'Red Hat Display';
+}
+.c-socials-item-link:hover .c-socials-item-label {
+  text-decoration: underline 2px;
+}
+.c-socials-item-link:focus .c-socials-item-label {
+  text-decoration: none;
+  background: var(--color-yellow);
+  box-shadow: 0 -2px var(--color-yellow),
+              0 4px var(--color-black);
+}
+
+.c-footerLink {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  height: 150px;
+  background: var(--color-light-gray);
+  border-top: 2px solid rgba(24, 24, 24, 0.025);
+}
+.c-footerLink-label {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+.c-footerLink-button {
+  display: block;
+  background: no-repeat center url('~/assets/ewbmq-logo-back.svg');
+  width: 200px;
+  height: 50px;
+}
+.c-footerLink-button:focus,
+.c-footerLink-button:active {
+  box-shadow: none;
+  background: no-repeat center url('~/assets/ewbmq-logo-back-focus.svg');
+}
+</style>
---
+++ pages/forms/join-gencomm.vue
@@ -0,0 +1,113 @@
+<template>
+  <main>
+    <div id="content" class="content">
+      <header class="c-header">
+        <h1 class="c-header-title">
+          <span class="c-header-subtitle">Nominations:</span>
+          Join the General Committee!
+        </h1>
+      </header>
+
+      <section class="c-section c-section--thinTopMargin c-section--thinBottomMargin c-section--thin">
+        <p>
+          <strong>Looking to join a dynamic, fast-paced & passionate team of people?</strong> We think the EWBMQ General Committee is just the place to be, whether it's as a Marketing Director, Events Director, School Outreach Coordinator, or as a GenComms Representative!
+        </p>
+        <p>
+          Fill out the form below by <strong>March 11th @ 11:59PM</strong> to submit your application for the desired position(s). We welcome people from diverse backgrounds, not just from within engineering!
+        </p>
+        <div class="c-info">
+          <strong>Need help?</strong> Feel free to ping the friendly EWBMQ team by <a href="mailto:macquarie@ewb.org.au" target="_blank" rel="noreferrer noopener" title="Compose a new email to EWBMQ in your default email application">sending us an email</a>.
+        </div>
+      </section>
+
+      <section class="c-section c-section--thinTopMargin c-section--thin">
+        <client-only>
+          <Form form-id="47642181-ae33-4f8b-bb3c-c1f2f42f20a4"/>
+        </client-only>
+      </section>
+
+      <section class="c-footerLink">
+        <NuxtLink to="/" class="c-footerLink-button">
+          <span class="c-footerLink-label">Go to EWBMQ homepage</span>
+        </NuxtLink>
+      </section>
+    </div>
+    <Footer />
+  </main>
+</template>
+
+<script>
+export default {
+  components: {
+    Form: () => process.client ? import(/* webpackPrefetch: true */ '~/lazy-components/Form.vue') : null
+  },
+  head: {
+    title: 'Applications Open: Join the General Committee! — EWBMQ',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Looking to join a dynamic, fast-paced & passionate team of people?' },
+      { property: 'twitter:title', name: 'twitter:title', content: 'Applications Open: Join the General Committee! — EWBMQ' },
+      { property: 'twitter:description', name: 'twitter:description', content: 'Looking to join a dynamic, fast-paced & passionate team of people?' },
+      { property: 'og:type', content: 'event' },
+      { property: 'og:title', name: 'og:title', content: 'Applications Open: Join the General Committee! — EWBMQ' },
+      { property: 'og:description', name: 'og:description', content: 'Looking to join a dynamic, fast-paced & passionate team of people?' },
+      { property: 'og:url', name: 'og:url', content: 'https://ewbmq.org/forms/join-gencomms-team/' }
+    ]
+  }
+}
+</script>
+
+<style scoped>
+.c-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  height: 175px;
+  background: var(--color-background);
+  box-shadow: 0 0 0 2px rgba(0, 0, 0, .05),
+              0 4px 32px rgba(0, 0, 0, .05);
+}
+.c-header-title {
+  font-size: 32px;
+  line-height: 48px;
+  text-align: center;
+}
+.c-header-subtitle {
+  display: block;
+  font-size: 20px;
+  line-height: 24px;
+  color: var(--color-green);
+}
+
+.c-footerLink {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  height: 150px;
+  background: var(--color-light-gray);
+  border-top: 2px solid rgba(24, 24, 24, 0.025);
+}
+.c-footerLink-label {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+.c-footerLink-button {
+  display: block;
+  background: no-repeat center url('~/assets/ewbmq-logo-back.svg');
+  width: 200px;
+  height: 50px;
+}
+.c-footerLink-button:focus,
+.c-footerLink-button:active {
+  box-shadow: none;
+  background: no-repeat center url('~/assets/ewbmq-logo-back-focus.svg');
+}
+</style>
